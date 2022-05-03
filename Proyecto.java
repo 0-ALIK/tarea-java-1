b240c43a15caf169b3c6eb1f197470ee948f62fd
Alik <moisesalik01@gmail.com>
2022-05-02 21:04:21 -0500

    Listo version 1.0

--- Proyecto.java
+++ Proyecto.java
@@ -1,13 +1,76 @@
+/*
+* Jugador
+    Atributos
+        nombre
+        puntosPartida
+        puntosTotales
+    Métodos
+        getNombre()
+        getPuntos()
+        getPuntosTotales()
+
+* Juego hereda de Jugador
+    Atributos
+        punto
+    Métodos
+        lanzarDado()
+*/
+
 import java.io.*;
 
 class Jugador
 {
-    
+    protected String jugador = "";
+    protected int puntosPartidas = 0;
+    protected int puntosTotales = 0;
+    Jugador(String jugador)
+    {
+        this.jugador = jugador;
+    }
+
+    public String getJugador() {
+        return jugador;
+    }
+
+    public int getPuntosPartidas() {
+        return puntosPartidas;
+    }
+
+    public int getPuntosTotales() {
+        return puntosTotales;
+    }
 }
 
-class Game
+class Juego extends Jugador
 {
+    private int punto;
+    public Juego (String jugador) 
+    {
+        super(jugador);
+        punto = 0;
+    }
 
+    public int getPunto() {
+        return punto;
+    }
+    
+    public int tirada()
+    {
+        punto = (int) (Math.random()*(6-1+1)+1);
+        return punto;
+    }
+
+    public void sumarPuntos() {
+        puntosPartidas  += punto;
+    }
+
+    public void reset () {
+        puntosPartidas = 0;
+    }
+
+    public void acumular() {
+        puntosTotales += puntosPartidas; 
+    }
 }
 
 public class Proyecto 
@@ -17,18 +80,54 @@ public class Proyecto
     {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         boolean activo = true;
+        System.out.print("\nIngrese el nombre del jugador 1: ");
+        Juego jugador1 = new Juego(br.readLine());
 
+        int tiradas;
+
+        System.out.print("\nIngrese el nombre del jugador 2: ");
+        Juego jugador2 = new Juego(br.readLine()); 
+        
         while(activo)
         {
             System.out.print("\n\nMenú\n 1. Si desea jugar\n2. Si desea ver los puntajes\n3. Si desea salir\nOpcion: ");
             switch(Integer.parseInt(br.readLine()))
             {
                 case 1:
+                    System.out.print("\nEl mejor de: ");
+                    tiradas = Integer.parseInt(br.readLine());
 
+                    for (int i = 1; i <= tiradas; i++) {
+                        System.out.print("\nVuela numero"+i+"\nEl jugador "+jugador1.getJugador()+"ha lanzado el dado y saco"+jugador1.tirada()+"puntos"); 
+                        System.out.print("\nEl jugador "+jugador2.getJugador()+"ha lanzado el dado y saco"+jugador2.tirada()+"puntos");
+
+                        jugador1.sumarPuntos(); jugador2.sumarPuntos();
+
+                        if(jugador1.getPunto() > jugador2.getPunto())
+                            System.out.print("\nEl jugador "+jugador1.getJugador()+" Se lleva la primera vuelta con: "+jugador1.getPunto()+" puntos");
+                        else
+                            System.out.print("\nEl jugador "+jugador2.getJugador()+" Se lleva la primera vuelta con: "+jugador2.getPunto()+" puntos");
+
+                        System.out.print("\nIngrese cualquier cosa para continuar con la siguiente vuelta: ");
+                        br.readLine();
+                    }
+
+                    jugador1.acumular(); jugador2.acumular();
+                    
+                    if(jugador1.getPuntosPartidas() > jugador2.getPuntosPartidas())
+                        System.out.print("\nEl jugador "+jugador1.getJugador()+" Ha ganado con "+jugador1.getPuntosPartidas()+" puntos");
+                    else
+                        System.out.print("\nEl jugador "+jugador2.getJugador()+" Ha ganado con "+jugador2.getPuntosPartidas()+" puntos");
+
+                    jugador1.reset();
+                    jugador2.reset();
                 break;
+                    
+
 
                 case 2:
-                    
+                    System.out.print("\nPuntos totales de "+jugador1.getJugador()+": "+jugador1.getPuntosTotales()+"\n");
+                    System.out.print("\nPuntos totales de "+jugador2.getJugador()+": "+jugador2.getPuntosTotales()+"\n");
                 break;
                 
                 case 3:
@@ -43,27 +142,3 @@ public class Proyecto
         }
     }
 }
-
-// Objetivos:Resolver problemas aplicando la propiedad de la herencia/polimorfismo. Comprender como la herencia fomenta la reutilización del software.Comprender los conceptos de clases base y clases derivadas.Aplicarlos efectos de los modos de acceso, constructores, sobrecarga, nombres de datos iguales y sobreescritura de métodos en la herencia. Enunciado:
-
-/* Escribe un programa en java para simular un juego de dados. Las reglas de juego del juego de  dados  conocido  son:  cada  dado  tiene  6  lados,  estos  lados  contienen  1,  2,  3,  4,  5,  6 puntos, después de lanzar los dos dados, se calcula la suma de los puntos. El jugador con más puntos gana; el empate es el mismo si los puntos son los mismos.Se describe a continuación ciertas recomendaciones.Considere el uso de varias clasesSolo son dos jugadores Debe contar los puntos para cada uno y determinar el ganadorControlar turnos y la jugadaMostrar los puntos del ganador  Uso de una herramienta colaborativa */
-
-/*
-    Jugador
-        atributos
-            nombre
-            puntosJuego
-            puntosPartida
-        métodos
-            getNombre()
-            getPuntos()
-            getAcumulados()
-    
-    Juego
-        atributos
-        
-            punto
-        métodos
-            lanzarDado()
-
-*/
